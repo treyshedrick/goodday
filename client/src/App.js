891cfe1200cdbe7e983c031561851960ff7f3f5a
treyshedrick <treyshedrick@gmail.com>
2021-01-27 19:06:04 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <p>Initial Commit</p>
+    </div>
+  );
+}
+
+export default App;
