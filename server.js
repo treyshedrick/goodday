1a1ac0052dd342e20c1d79228438f52d7fab23a6
treyshedrick <treyshedrick@gmail.com>
2021-01-27 20:26:24 -0600

    install cors, axios, and yarn

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
