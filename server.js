ed7eb8cabf1151539d70c7342b3e9459bdafaef7
treyshedrick <treyshedrick@gmail.com>
2021-01-31 16:27:48 -0600

    ignore dbconfigs and connect to db with test query

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser');
+const config = require('./config')
+const { Client } = require('pg')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,11 +27,13 @@ app.get('/api/zenquote', (req, res) => {
   res.send({ quote });
 });
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+app.get('/api/users', (req, res) => {
+const client = new Client(config.prod)
+client.connect()
+client.query('SELECT * FROM AppUser', (err, dbres) => {
+  res.send(dbres.rows[0].firstname)
+  client.end()
+})
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
