0cb0886e32ff0a139017901661595db89a188531
treyshedrick <treyshedrick@gmail.com>
2021-02-14 10:37:27 -0600

    Post.js form logic

--- client/src/components/Main/Home.js
+++ client/src/components/Main/Home.js
@@ -2,14 +2,13 @@ import Post from './Post';
 import Task from './Task';
 
 let Home = (props) => {
-    console.log(props.user)
     return(
         <div className="main container">
             <div className="row">
                 <div className="col-12">Hello {props.user.name}</div>
             </div>
             <div className="row">
-                <div className="col-12"><Post /></div>
+                <div className="col-12"><Post user={props.user}/></div>
             </div>
             <div className="row">
                 <div className="col-12"><Task /></div>
--- client/src/components/Main/Post.js
+++ client/src/components/Main/Post.js
@@ -1,16 +1,59 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import axios from 'axios'
 
 class Post extends Component{
     constructor(props){
         super(props)
         this.state = {
-            
+            positivePost: "",
+            submit: false,
+            posted: false
         }
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.positivePost === prevState.positivePost && !this.state.posted && this.state.submit){
+            let post = {id: this.props.user.id, post: this.state.positivePost}
+            console.log("Post should only occur once")
+            axios.post('http://localhost:5000/api/post', post)
+            .then(response =>{
+                console.log(response)
+                this.setState({
+                    posted: true
+                })
+            })
+            .catch(axiosErr =>{
+                console.log(axiosErr)
+            })
+        }
+    }
+
+    handleChange(e){
+        this.setState({
+            positivePost: e.target.value
+        })
+        e.preventDefault();
+    }
+
+    handleSubmit(e){
+        this.setState({
+            submit: true
+        })
+        e.preventDefault();
     }
 
     render(){
         return(
-            <div>Something Positive</div>
+            <div>
+                <div>Something Positive</div>
+                <form onSubmit={this.handleSubmit}>
+                    <textarea placeholder="Say something positive about the day!" onChange={this.handleChange}></textarea>
+                    <button type="submit">Post!</button>                
+                </form>
+            </div>
         )
     }
 }
